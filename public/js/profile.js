cb365b47656336657c038730c3a824dde6b40bfb
zerobit <orourke_kyle@icloud.com>
2020-09-04 11:17:12 -0400

    updating the little things

--- public/js/profile.js
+++ public/js/profile.js
@@ -4,6 +4,7 @@ $(document).ready(() => {
   var container = $("#boughtCont");
   var listDiv = $("#shoeList")
   $(document).on("click", ".sellBtn", handleDeleteButtonPress);
+  
   // This file just does a GET request to figure out which user is logged in
   // and updates the HTML on the page
   $.get("/api/user_data").then(data => {
